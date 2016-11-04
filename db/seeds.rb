5d232a52a408d992f1be1c7610b68bc2a908b237
Andrew <andrew@huth.me>
2016-11-03 18:49:39 -0700

    Ensure the the DB seeds don't are indempotent
    Meaning, when running multiple times they should not duplicate data.
    Note that when creating Jane we can’t use `find_or_create_by`. That’s
    because `password` is not actually a field on the model.
    
    Finally, there was also an error when creating responses. We can’t
    directly set `seat`. We have to set `seat_id`.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,18 @@
 if Rails.env.development?
   jane = User.create(name: 'Jane', email: 'jane@example.com', password: 'password', password_confirmation: 'password')
-  blue_team = Team.create(name: 'Blue', user_id: jane.id)
-  red_team = Team.create(name: 'Red', user_id: jane.id)
-  Seat.create(name: 'Adam', email: 'adam@example.com', team_id: blue_team.id)
-  Seat.create(name: 'Eric', email: 'eric@example.com', team_id: blue_team.id)
-  ryan_seat = Seat.create(name: 'Ryan', email: 'ryan@example.com', team_id: blue_team.id)
-  shirish_seat = Seat.create(name: 'Shirish', email: 'shirish@example.com', team_id: blue_team.id)
-  Seat.create(name: 'Goose', email: 'goose@example.com', team_id: red_team.id)
-  Seat.create(name: 'Iceman', email: 'iceman@example.com', team_id: red_team.id)
-  Seat.create(name: 'Maverick', email: 'maverick@example.com', team_id: red_team.id)
-  Response.create(handled: false, seat: shirish_seat, body: <<~EOS)
+  blue_team = Team.find_or_create_by(name: 'Blue', user_id: jane.id)
+  red_team = Team.find_or_create_by(name: 'Red', user_id: jane.id)
+  Seat.find_or_create_by(name: 'Adam', email: 'adam@example.com', team_id: blue_team.id)
+  Seat.find_or_create_by(name: 'Eric', email: 'eric@example.com', team_id: blue_team.id)
+  ryan_seat = Seat.find_or_create_by(name: 'Ryan', email: 'ryan@example.com', team_id: blue_team.id)
+  shirish_seat = Seat.find_or_create_by(name: 'Shirish', email: 'shirish@example.com', team_id: blue_team.id)
+  Seat.find_or_create_by(name: 'Goose', email: 'goose@example.com', team_id: red_team.id)
+  Seat.find_or_create_by(name: 'Iceman', email: 'iceman@example.com', team_id: red_team.id)
+  Seat.find_or_create_by(name: 'Maverick', email: 'maverick@example.com', team_id: red_team.id)
+  Response.find_or_create_by(handled: false, seat_id: shirish_seat.id, body: <<~EOS)
     Beat Eric at Ping-Pong!
   EOS
-  Response.create(handled: false, seat: ryan_seat, body: <<~EOS)
+  Response.find_or_create_by(handled: false, seat_id: ryan_seat.id, body: <<~EOS)
     - Started expense sync
     - Deployed to staging
   EOS
