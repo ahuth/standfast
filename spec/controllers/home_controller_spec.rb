84cd08c510fc23fb6a609901afd19a1495901ef5
Andrew <andrew@huth.me>
2016-11-03 23:35:59 -0700

    Redirect from the home#index page to teams#index if logged in

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -6,9 +6,25 @@ describe HomeController, type: :controller do
       get :index
     end
 
-    it "is successful" do
-      do_request
-      expect(response).to have_http_status(:success)
+    context "when a user is not logged in" do
+      before do
+        expect(subject.current_user).to be_nil
+      end
+
+      it "is successful" do
+        do_request
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context "when a user is logged in" do
+      let(:user) { users(:jane) }
+
+      it "redirects to the teams page" do
+        sign_in(user)
+        do_request
+        expect(response).to redirect_to(teams_path)
+      end
     end
   end
 end
