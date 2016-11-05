5c1be3c969fe8953816363c52cee4cc2e854da58
Andrew <andrew@huth.me>
2016-11-05 07:56:55 -0700

    Add route for updating a seat

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -1,13 +1,25 @@
 class SeatsController < ApplicationController
   before_action :authenticate_user!
-  before_action :load_seat, only: [:edit]
+  before_action :load_seat, only: [:edit, :update]
 
   def edit
+  end
+
+  def update
+    if @seat.update(seat_params)
+      redirect_to team_path(@seat.team), notice: "Seat was succesfully updated"
+    else
+      render action: "edit"
+    end
   end
 
   private
 
   def load_seat
     @seat = current_user.seats.find(params[:id])
+  end
+
+  def seat_params
+    params.require(:seat).permit(:email)
   end
 end
--- spec/controllers/seats_controller_spec.rb
+++ spec/controllers/seats_controller_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 require 'support/shared_examples/controllers'
 
 describe SeatsController, type: :controller do
-  describe "#edit" do
-    let(:user) { users(:bob) }
+  let(:user) { users(:bob) }
 
+  describe "#edit" do
     def do_request
       get :edit, params: { id: seat.id }
     end
@@ -29,6 +29,66 @@ describe SeatsController, type: :controller do
         sign_in(user)
         do_request
         expect(response).to have_http_status(:success)
+      end
+    end
+  end
+
+  describe "#update" do
+    def do_request
+      patch :update, params: { id: seat.id, seat: seat_params }
+    end
+
+    it_behaves_like "login is required" do
+      let(:seat) { seats(:bob_pink_team_bill_seat) }
+      let(:seat_params) { {} }
+    end
+
+    it_behaves_like "resource ownership is required" do
+      let(:seat) { seats(:jane_red_team_iceman_seat) }
+      let(:seat_params) { {} }
+      let(:resource_owner_id) { seat.team.user_id }
+    end
+
+    context "when the seat belongs to the user" do
+      let(:seat) { seats(:bob_pink_team_bill_seat) }
+
+      before do
+        expect(seat.team.user_id).to eq(user.id)
+      end
+
+      context "when sending valid params" do
+        let(:seat_params) { { email: "aaa@example.com" } }
+
+        it "redirects back to the team" do
+          sign_in(user)
+          do_request
+          expect(response).to redirect_to(team_path(seat.team))
+        end
+
+        it "updates the model" do
+          expect(seat.email).to_not eq("aaa@example.com")
+          sign_in(user)
+          do_request
+          seat.reload
+          expect(seat.email).to eq("aaa@example.com")
+        end
+      end
+
+      context "when sending invalid params" do
+        let(:seat_params) { { email: "" } }
+
+        it "is successful" do
+          sign_in(user)
+          do_request
+          expect(response).to have_http_status(:success)
+        end
+
+        it "does not update the model" do
+          sign_in(user)
+          do_request
+          seat.reload
+          expect(seat.email).to eq("bill@example.com")
+        end
       end
     end
   end
