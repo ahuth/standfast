2985735a050bcc93c8afaae49a11bffc078427e1
Andrew <andrew@huth.me>
2016-11-05 18:54:13 -0700

    Extract variables/methods where it makes sense
    In order to DRY up the tests

--- spec/controllers/seats_controller_spec.rb
+++ spec/controllers/seats_controller_spec.rb
@@ -3,48 +3,50 @@ require 'support/shared_examples/controllers'
 
 describe SeatsController, type: :controller do
   let(:user) { users(:bob) }
+  let(:owned_seat) { seats(:bob_pink_team_bill_seat) }
+  let(:non_owned_seat) { seats(:jane_red_team_iceman_seat) }
 
   describe "#new" do
     it_behaves_like "a protected new action" do
-      let(:owner_request_params) { { team_id: teams(:bob_black_team).id } }
-      let(:non_owner_request_params) { { team_id: teams(:jane_red_team).id } }
+      let(:owner_request_params) { { team_id: owned_seat.team_id } }
+      let(:non_owner_request_params) { { team_id: non_owned_seat.team_id } }
     end
   end
 
   describe "#create" do
+    let(:valid_seat_params) { { name: "Buzz", email: "buzz@example.com" } }
+    let(:invalid_seat_params) { { email: "buzz@example.com" } }
+
     it_behaves_like "a protected create action" do
       let(:model) { Seat }
-      let(:valid_owner_request_params) { { team_id: teams(:bob_pink_team).id, seat: { name: "Buzz", email: "buzz@example.com" } } }
-      let(:valid_non_owner_request_params) { { team_id: teams(:jane_red_team).id, seat: { name: "Buzz", email: "buzz@example.com" } } }
-      let(:invalid_owner_request_params) { { team_id: teams(:bob_pink_team).id, seat: { email: "buzz@example.com" } } }
+      let(:valid_owner_request_params) { { team_id: owned_seat.team_id, seat: valid_seat_params } }
+      let(:valid_non_owner_request_params) { { team_id: non_owned_seat.team_id, seat: valid_seat_params } }
+      let(:invalid_owner_request_params) { { team_id: owned_seat.team_id, seat: invalid_seat_params } }
       let(:after_create_redirect_url) { team_path(teams(:bob_pink_team)) }
     end
   end
 
   describe "#edit" do
     it_behaves_like "a protected edit action" do
-      let(:owner_request_params) { { id: seats(:bob_pink_team_bill_seat).id } }
-      let(:non_owner_request_params) { { id: seats(:jane_red_team_iceman_seat).id } }
+      let(:owner_request_params) { { id: owned_seat.id } }
+      let(:non_owner_request_params) { { id: non_owned_seat.id } }
     end
   end
 
   describe "#update" do
-    let(:owned_seat) { seats(:bob_pink_team_bill_seat) }
-    let(:non_owned_seat) { seats(:jane_red_team_iceman_seat) }
+    let(:valid_seat_params) { { email: "aaa@example.com" } }
+    let(:invalid_seat_params) { { email: "" } }
 
     it_behaves_like "a protected update action" do
       let(:model_updated?) { -> { owned_seat.reload.email == "aaa@example.com" } }
-      let(:valid_owner_request_params) { { id: owned_seat.id, seat: { email: "aaa@example.com" } } }
-      let(:valid_non_owner_request_params) { { id: non_owned_seat.id, seat: { email: "aaa@example.com" } } }
-      let(:invalid_owner_request_params) { { id: owned_seat.id, seat: { email: "" } } }
+      let(:valid_owner_request_params) { { id: owned_seat.id, seat: valid_seat_params } }
+      let(:valid_non_owner_request_params) { { id: non_owned_seat.id, seat: valid_seat_params } }
+      let(:invalid_owner_request_params) { { id: owned_seat.id, seat: invalid_seat_params } }
       let(:after_update_redirect_url) { team_path(owned_seat.team) }
     end
   end
 
   describe "#destroy" do
-    let(:owned_seat) { seats(:bob_pink_team_bill_seat) }
-    let(:non_owned_seat) { seats(:jane_red_team_iceman_seat) }
-
     it_behaves_like "a protected destroy action" do
       let(:model) { Seat }
       let(:owner_request_params) { { id: owned_seat.id } }
--- spec/controllers/teams_controller_spec.rb
+++ spec/controllers/teams_controller_spec.rb
@@ -3,6 +3,8 @@ require 'support/shared_examples/controllers'
 
 describe TeamsController, type: :controller do
   let(:user) { users(:jane) }
+  let(:owned_team) { teams(:jane_blue_team) }
+  let(:non_owned_team) { teams(:bob_black_team) }
 
   describe "#index" do
     it_behaves_like "a protected index action"
@@ -10,8 +12,8 @@ describe TeamsController, type: :controller do
 
   describe "#show" do
     it_behaves_like "a protected show action" do
-      let(:object_owned_by_user) { teams(:jane_red_team) }
-      let(:object_not_owned_by_user) { teams(:bob_pink_team) }
+      let(:object_owned_by_user) { owned_team }
+      let(:object_not_owned_by_user) { non_owned_team }
     end
   end
 
@@ -22,38 +24,38 @@ describe TeamsController, type: :controller do
   end
 
   describe "#create" do
+    let(:valid_team_params) { { name: "Purple" } }
+    let(:invalid_team_params) { { name: "" } }
+
     it_behaves_like "a protected create action", skip_ownership_check: true do
       let(:model) { Team }
-      let(:valid_owner_request_params) { { team: { name: "Purple" } } }
-      let(:invalid_owner_request_params) { { team: { name: "" } } }
+      let(:valid_owner_request_params) { { team: valid_team_params } }
+      let(:invalid_owner_request_params) { { team: invalid_team_params } }
       let(:after_create_redirect_url) { teams_path }
     end
   end
 
   describe "#edit" do
     it_behaves_like "a protected edit action" do
-      let(:owner_request_params) { { id: teams(:jane_blue_team).id } }
-      let(:non_owner_request_params) { { id: teams(:bob_black_team).id } }
+      let(:owner_request_params) { { id: owned_team.id } }
+      let(:non_owner_request_params) { { id: non_owned_team.id } }
     end
   end
 
   describe "update" do
-    let(:owned_team) { teams(:jane_blue_team) }
-    let(:non_owned_team) { teams(:bob_black_team) }
+    let(:valid_team_params) { { name: "Green" } }
+    let(:invalid_team_params) { { name: "" } }
 
     it_behaves_like "a protected update action" do
       let(:model_updated?) { -> { owned_team.reload.name == "Green" } }
-      let(:valid_owner_request_params) { { id: owned_team.id, team: { name: "Green" } } }
-      let(:valid_non_owner_request_params) { { id: non_owned_team.id, team: { name: "Green" } } }
-      let(:invalid_owner_request_params) { { id: owned_team.id, team: { name: "" } } }
+      let(:valid_owner_request_params) { { id: owned_team.id, team: valid_team_params } }
+      let(:valid_non_owner_request_params) { { id: non_owned_team.id, team: valid_team_params } }
+      let(:invalid_owner_request_params) { { id: owned_team.id, team: invalid_team_params } }
       let(:after_update_redirect_url) { team_path(owned_team) }
     end
   end
 
   describe "#destroy" do
-    let(:owned_team) { teams(:jane_blue_team) }
-    let(:non_owned_team) { teams(:bob_black_team) }
-
     it_behaves_like "a protected destroy action" do
       let(:model) { Team }
       let(:owner_request_params) { { id: owned_team.id } }
