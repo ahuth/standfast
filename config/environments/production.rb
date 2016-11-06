ed55ebe28656808e64ad3ea6b99b0b0436419bac
Andrew <andrew@huth.me>
2016-11-06 13:36:10 -0800

    Use `heroku.com` for the domain
    Not sure if it matters or not

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name: ENV["SENDGRID_USERNAME"],
     password: ENV["SENDGRID_PASSWORD"],
-    domain: "standfastapp.com",
+    domain: "heroku.com",
     address: "smtp.sendgrid.net",
     port: 587,
     authentication: :login,
