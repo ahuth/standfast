1fe4be04e1bbc030ab7887e1471492cb18089563
Andrew <andrew@huth.me>
2016-11-07 22:18:53 -0800

    Make `root` the first entry in the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "home#index"
+  devise_for :users
 
   resources :teams, shallow: true do
     resources :seats, except: [:index, :show]
