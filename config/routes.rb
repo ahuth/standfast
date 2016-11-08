c03af7f4ba1b53f118f4e4f7b858e61284b37ee6
Andrew <andrew@huth.me>
2016-11-07 22:19:40 -0800

    Add Griddler route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "home#index"
   devise_for :users
+  mount_griddler
 
   resources :teams, shallow: true do
     resources :seats, except: [:index, :show]
