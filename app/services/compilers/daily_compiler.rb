c85061ee7657b5af7235a7cb63ae11ccb5c0bb2f
Andrew <andrew@huth.me>
2016-11-09 21:24:49 -0800

    Go simple

--- app/services/compilers/daily_compiler.rb
+++ app/services/compilers/daily_compiler.rb
@@ -5,7 +5,7 @@ module Compilers
         teams_with_unhandled_responses.find_each do |team|
           SummaryMailer.daily_summary_email(team).deliver_later
         end
-        responses_from_unhandled_teams.where(handled: false).update_all(handled: true)
+        Response.where(handled: false).update_all(handled: true)
       end
     end
 
@@ -18,17 +18,7 @@ module Compilers
       Team.joins(:responses).where("responses.handled = false")
     end
 
-    def self.seats_from_unhandled_teams
-      Seat.joins(:team).where("team_id in (?)", teams_with_unhandled_responses.pluck(:id))
-    end
-
-    def self.responses_from_unhandled_teams
-      Response.joins(:seat).where("seat_id in (?)", seats_from_unhandled_teams.pluck(:id))
-    end
-
     private_class_method :pacific_weekend_in_utc?
     private_class_method :teams_with_unhandled_responses
-    private_class_method :seats_from_unhandled_teams
-    private_class_method :responses_from_unhandled_teams
   end
 end
