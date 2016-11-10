54e07311aa5024b30a2087992bb5ea3f9c14d746
Andrew <andrew@huth.me>
2016-11-09 21:22:05 -0800

    Avoid the n+1 query at all cost
    Even though this is nastier (and makes several more queries), it’s
    faster in the tests.

--- app/services/compilers/daily_compiler.rb
+++ app/services/compilers/daily_compiler.rb
@@ -4,8 +4,8 @@ module Compilers
       if !pacific_weekend_in_utc?
         teams_with_unhandled_responses.find_each do |team|
           SummaryMailer.daily_summary_email(team).deliver_later
-          team.responses.where(handled: false).update_all(handled: true)
         end
+        responses_from_unhandled_teams.where(handled: false).update_all(handled: true)
       end
     end
 
@@ -18,7 +18,17 @@ module Compilers
       Team.joins(:responses).where("responses.handled = false")
     end
 
+    def self.seats_from_unhandled_teams
+      Seat.joins(:team).where("team_id in (?)", teams_with_unhandled_responses.pluck(:id))
+    end
+
+    def self.responses_from_unhandled_teams
+      Response.joins(:seat).where("seat_id in (?)", seats_from_unhandled_teams.pluck(:id))
+    end
+
     private_class_method :pacific_weekend_in_utc?
     private_class_method :teams_with_unhandled_responses
+    private_class_method :seats_from_unhandled_teams
+    private_class_method :responses_from_unhandled_teams
   end
 end
