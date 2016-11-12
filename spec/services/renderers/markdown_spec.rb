ac05256a9150803260183961a94278cef804b3d6
Andrew <andrew@huth.me>
2016-11-12 10:20:44 -0800

    Process prompt responses as markdown

---
+++ spec/services/renderers/markdown_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+describe Renderers::Markdown do
+  describe ".process" do
+    let(:processed) { described_class.process(input) }
+
+    context "with safe input" do
+      let(:input) do
+        <<~EOS
+          - First
+          - Second
+        EOS
+      end
+
+      it "converts to html" do
+        expect(processed).to eq(<<~EOS)
+          <ul>
+          <li>First</li>
+          <li>Second</li>
+          </ul>
+        EOS
+      end
+    end
+
+    context "with unsafe input" do
+      let(:input) { '<script>alert("Hacked!")</script>' }
+
+      it "escapes the input" do
+        expect(processed).to eq("<p>alert(&quot;Hacked!&quot;)</p>\n")
+      end
+    end
+  end
+end
