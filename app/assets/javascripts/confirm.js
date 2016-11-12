1c41ce4b497c8eb8c70f3655b4c91c4c557b1a49
Andrew <andrew@huth.me>
2016-11-12 03:34:20 -0800

    Use `var` for backwards compatibility

--- app/assets/javascripts/confirm.js
+++ app/assets/javascripts/confirm.js
@@ -1,5 +1,5 @@
 document.addEventListener("turbolinks:load", function (event) {
-  const elements = document.querySelectorAll("[data-confirm]")
+  var elements = document.querySelectorAll("[data-confirm]");
   elements.forEach(function (element) {
     element.onclick = function (event) {
       return window.confirm(event.target.dataset.confirm);
