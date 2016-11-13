5edf23d7c615e6ccf5bfe7b4543abea835cb3942
Andrew <andrew@huth.me>
2016-11-12 17:50:19 -0800

    Use a marketing layout for the home page
    This layout has no JS and should load faster.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
-    redirect_to(teams_path) if user_signed_in?
+    if user_signed_in?
+      redirect_to(teams_path)
+    else
+      render layout: "marketing"
+    end
   end
 end
