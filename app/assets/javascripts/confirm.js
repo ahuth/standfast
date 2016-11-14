f20878f4b401f39a82d456a4ac8c484429dc4c89
Andrew <andrew@huth.me>
2016-11-13 16:57:13 -0800

    Add comment to the data-confirm click handler

--- app/assets/javascripts/confirm.js
+++ app/assets/javascripts/confirm.js
@@ -1,3 +1,6 @@
+// For every element with a `data-confirm` attribute, show a confirmation dialog
+// after clicking. The value of the `data-confirm` attribute will be shown in
+// the dialog, and canceling should prevent the action from taking place.
 document.addEventListener("turbolinks:load", function (event) {
   var elements = document.querySelectorAll("[data-confirm]");
   elements.forEach(function (element) {
