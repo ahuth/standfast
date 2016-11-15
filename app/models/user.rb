4d322e234d280845b741ccc7d88f0d4d7df1be9b
Andrew <andrew@huth.me>
2016-11-14 22:12:09 -0800

    Ensure that an account has at least one user
    Also, rename the `describe` block for creating users to be consistent
    with our new tests for destroying them.

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,20 @@ class User < ApplicationRecord
   belongs_to :account
 
   before_validation :create_account, on: :create
+  before_destroy :ensure_account_has_users
 
   private
 
   def create_account
     if account.blank?
       self.account = Account.create!
+    end
+  end
+
+  def ensure_account_has_users
+    if account.users.count == 1
+      errors.add(:base, "Cannot remove the only user on an account")
+      throw :abort
     end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -44,7 +44,7 @@ describe User, type: :model do
     end
   end
 
-  describe "creating a new one" do
+  describe "creating" do
     before do
       user.save!
     end
@@ -63,6 +63,38 @@ describe User, type: :model do
 
       it "creates a new account" do
         expect(user.account).to be_present
+      end
+    end
+  end
+
+  describe "destroying" do
+    context "when more than one user is on the account" do
+      let(:user) { users(:bob) }
+
+      before do
+        expect(user.account.users.count).to be > 1
+        user.destroy
+      end
+
+      it "works" do
+        expect(user).to be_destroyed
+      end
+    end
+
+    context "when the user is the only one on the account" do
+      let(:user) { users(:jane) }
+
+      before do
+        expect(user.account.users.count).to eq(1)
+        user.destroy
+      end
+
+      it "does not work" do
+        expect(user).to_not be_destroyed
+      end
+
+      it "adds a helpful error message" do
+        expect(user.errors.full_messages).to eq(["Cannot remove the only user on an account"])
       end
     end
   end
