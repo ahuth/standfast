50c7801fa5675b9cd7496dc6e99b498dd254d6b0
Andrew <andrew@huth.me>
2016-11-17 19:30:05 -0800

    Allow passing multiple test paths to the "active" JS handler

--- app/assets/javascripts/active.js
+++ app/assets/javascripts/active.js
@@ -4,9 +4,11 @@ document.addEventListener("turbolinks:load", function (event) {
   var elements = document.querySelectorAll("[data-active]");
   var path = window.location.pathname;
   elements.forEach(function (element) {
-    var pattern = new RegExp(element.dataset.active + ".*");
+    var paths = element.dataset.active.split(",");
+    var patterns = paths.map(function (path) { return path + ".*"; });
+    var pattern = new RegExp(patterns.join("|"));
     if (pattern.test(path)) {
-      element.classList.add("active")
+      element.classList.add("active");
     }
   });
 });
