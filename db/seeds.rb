16d2aa0f15272af8dfe059dbcc22e3e73c348989
Andrew <andrew@huth.me>
2016-11-19 08:59:23 -0800

    Make the seeds more indempotent
    First, make sure we don’t create a new account and new user every time
    the seeds run. If that happens, then everything else will also be
    created again. Also, we have to do a little bit more work to create the
    user, since the “password” isn’t a real DB field.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,39 @@
 if Rails.env.development?
   # Account
-  account = Account.create
+  account = Account.first || Account.create!
 
   # User
-  jane = User.create(account_id: account.id, email: 'jane@example.com', password: 'password', password_confirmation: 'password')
+  jane = User.find_or_initialize_by({
+    account_id: account.id,
+    email: 'jane@example.com'
+  })
+  jane.password = 'password'
+  jane.encrypted_password = User.new.send(:password_digest, 'password')
+  jane.save!
 
   # Teams
-  blue_team = Team.find_or_create_by(name: 'Blue', account_id: account.id)
-  red_team = Team.find_or_create_by(name: 'Red', account_id: account.id)
+  blue_team = Team.find_or_create_by!(name: 'Blue', account_id: account.id)
+  red_team = Team.find_or_create_by!(name: 'Red', account_id: account.id)
 
   # Seats
-  Seat.find_or_create_by(name: 'Adam', email: 'adam@example.com', team_id: blue_team.id)
-  Seat.find_or_create_by(name: 'Eric', email: 'eric@example.com', team_id: blue_team.id)
-  ryan_seat = Seat.find_or_create_by(name: 'Ryan', email: 'ryan@example.com', team_id: blue_team.id)
-  shirish_seat = Seat.find_or_create_by(name: 'Shirish', email: 'shirish@example.com', team_id: blue_team.id)
-  anna_seat = Seat.find_or_create_by(name: 'Anna', email: 'anna@example.com', team_id: blue_team.id)
-  Seat.find_or_create_by(name: 'Goose', email: 'goose@example.com', team_id: red_team.id)
-  Seat.find_or_create_by(name: 'Iceman', email: 'iceman@example.com', team_id: red_team.id)
-  Seat.find_or_create_by(name: 'Maverick', email: 'maverick@example.com', team_id: red_team.id)
+  Seat.find_or_create_by!(name: 'Adam', email: 'adam@example.com', team_id: blue_team.id)
+  Seat.find_or_create_by!(name: 'Eric', email: 'eric@example.com', team_id: blue_team.id)
+  ryan_seat = Seat.find_or_create_by!(name: 'Ryan', email: 'ryan@example.com', team_id: blue_team.id)
+  shirish_seat = Seat.find_or_create_by!(name: 'Shirish', email: 'shirish@example.com', team_id: blue_team.id)
+  anna_seat = Seat.find_or_create_by!(name: 'Anna', email: 'anna@example.com', team_id: blue_team.id)
+  Seat.find_or_create_by!(name: 'Goose', email: 'goose@example.com', team_id: red_team.id)
+  Seat.find_or_create_by!(name: 'Iceman', email: 'iceman@example.com', team_id: red_team.id)
+  Seat.find_or_create_by!(name: 'Maverick', email: 'maverick@example.com', team_id: red_team.id)
 
   # Responses
-  Response.find_or_create_by(handled: false, seat_id: shirish_seat.id, body: <<~EOS)
+  Response.find_or_create_by!(handled: false, seat_id: shirish_seat.id, body: <<~EOS)
     Beat Eric at Ping-Pong!
   EOS
-  Response.find_or_create_by(handled: false, seat_id: ryan_seat.id, body: <<~EOS)
+  Response.find_or_create_by!(handled: false, seat_id: ryan_seat.id, body: <<~EOS)
     - Started expense sync
     - Deployed to staging
   EOS
-  Response.find_or_create_by(handled: false, seat_id: anna_seat.id, body: <<~EOS)
+  Response.find_or_create_by!(handled: false, seat_id: anna_seat.id, body: <<~EOS)
     Finished the machine learning algorithm.
   EOS
 
