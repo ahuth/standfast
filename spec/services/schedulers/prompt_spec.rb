5205cde5941c47ba9a9c4e31887e0dbcb6f9c6bf
Andrew <andrew@huth.me>
2016-11-20 10:31:04 -0800

    Remove unnecessary tests from the prompt and summary schedulers
    The nitty-gritty of scheduling is tested in the Weekday scheduler. Here
    we just want some basic, high-level tests to ensure that everything is
    working at a behavioral / integration level.

--- spec/services/schedulers/prompt_spec.rb
+++ spec/services/schedulers/prompt_spec.rb
@@ -7,7 +7,7 @@ describe Schedulers::Prompt do
   describe ".run" do
     let(:teams_count) { Team.count }
 
-    context "on Friday" do
+    context "on a weekday" do
       context "at 4pm" do
         before do
           travel_to Time.utc(2016, 11, 19, 0) do
@@ -33,21 +33,9 @@ describe Schedulers::Prompt do
           expect(enqueued_jobs.last[:args].first).to eq("PromptMailer")
         end
       end
-
-      context "at 6pm" do
-        before do
-          travel_to Time.utc(2016, 11, 19, 2) do
-            described_class.run
-          end
-        end
-
-        it "does not send daily prompts" do
-          expect(enqueued_jobs.count).to eq(0)
-        end
-      end
     end
 
-    context "on Saturday" do
+    context "on a weekend" do
       context "at 4pm" do
         before do
           travel_to Time.utc(2016, 11, 20, 0) do
@@ -63,96 +51,6 @@ describe Schedulers::Prompt do
       context "at 5pm" do
         before do
           travel_to Time.utc(2016, 11, 20, 1) do
-            described_class.run
-          end
-        end
-
-        it "does not send daily prompts" do
-          expect(enqueued_jobs.count).to eq(0)
-        end
-      end
-
-      context "at 6pm" do
-        before do
-          travel_to Time.utc(2016, 11, 20, 2) do
-            described_class.run
-          end
-        end
-
-        it "does not send daily prompts" do
-          expect(enqueued_jobs.count).to eq(0)
-        end
-      end
-    end
-
-    context "on Sunday" do
-      context "at 4pm" do
-        before do
-          travel_to Time.utc(2016, 11, 21, 0) do
-            described_class.run
-          end
-        end
-
-        it "does not send daily prompts" do
-          expect(enqueued_jobs.count).to eq(0)
-        end
-      end
-
-      context "at 5pm" do
-        before do
-          travel_to Time.utc(2016, 11, 21, 1) do
-            described_class.run
-          end
-        end
-
-        it "does not send daily prompts" do
-          expect(enqueued_jobs.count).to eq(0)
-        end
-      end
-
-      context "at 6pm" do
-        before do
-          travel_to Time.utc(2016, 11, 21, 2) do
-            described_class.run
-          end
-        end
-
-        it "does not send daily prompts" do
-          expect(enqueued_jobs.count).to eq(0)
-        end
-      end
-    end
-
-    context "on Monday" do
-      context "at 4pm" do
-        before do
-          travel_to Time.utc(2016, 11, 22, 0) do
-            described_class.run
-          end
-        end
-
-        it "does not send daily prompts" do
-          expect(enqueued_jobs.count).to eq(0)
-        end
-      end
-
-      context "at 5pm" do
-        before do
-          travel_to Time.utc(2016, 11, 22, 1) do
-            described_class.run
-          end
-        end
-
-        it "sends a daily prompt for each team" do
-          expect(teams_count).to be > 0
-          expect(enqueued_jobs.count).to eq(teams_count)
-          expect(enqueued_jobs.last[:args].first).to eq("PromptMailer")
-        end
-      end
-
-      context "at 6pm" do
-        before do
-          travel_to Time.utc(2016, 11, 22, 2) do
             described_class.run
           end
         end
--- spec/services/schedulers/summary_spec.rb
+++ spec/services/schedulers/summary_spec.rb
@@ -7,7 +7,7 @@ describe Schedulers::Summary do
   describe ".run" do
     let!(:unhandled_teams_count) { Team.joins(:responses).where("responses.handled = false").distinct.count }
 
-    context "on Friday" do
+    context "on a weekday" do
       context "at 6am" do
         before do
           travel_to Time.utc(2016, 11, 18, 14) do
@@ -33,21 +33,9 @@ describe Schedulers::Summary do
           expect(enqueued_jobs.last[:args].first).to eq("SummaryMailer")
         end
       end
-
-      context "at 8am" do
-        before do
-          travel_to Time.utc(2016, 11, 18, 16) do
-            described_class.run
-          end
-        end
-
-        it "does not send daily prompts" do
-          expect(enqueued_jobs.count).to eq(0)
-        end
-      end
     end
 
-    context "on Saturday" do
+    context "on a weekend" do
       context "at 6am" do
         before do
           travel_to Time.utc(2016, 11, 19, 14) do
@@ -63,96 +51,6 @@ describe Schedulers::Summary do
       context "at 7am" do
         before do
           travel_to Time.utc(2016, 11, 19, 15) do
-            described_class.run
-          end
-        end
-
-        it "does not send daily prompts" do
-          expect(enqueued_jobs.count).to eq(0)
-        end
-      end
-
-      context "at 8am" do
-        before do
-          travel_to Time.utc(2016, 11, 19, 16) do
-            described_class.run
-          end
-        end
-
-        it "does not send daily prompts" do
-          expect(enqueued_jobs.count).to eq(0)
-        end
-      end
-    end
-
-    context "on Sunday" do
-      context "at 6am" do
-        before do
-          travel_to Time.utc(2016, 11, 20, 14) do
-            described_class.run
-          end
-        end
-
-        it "does not send daily prompts" do
-          expect(enqueued_jobs.count).to eq(0)
-        end
-      end
-
-      context "at 7am" do
-        before do
-          travel_to Time.utc(2016, 11, 20, 15) do
-            described_class.run
-          end
-        end
-
-        it "does not send daily prompts" do
-          expect(enqueued_jobs.count).to eq(0)
-        end
-      end
-
-      context "at 8am" do
-        before do
-          travel_to Time.utc(2016, 11, 20, 16) do
-            described_class.run
-          end
-        end
-
-        it "does not send daily prompts" do
-          expect(enqueued_jobs.count).to eq(0)
-        end
-      end
-    end
-
-    context "on Monday" do
-      context "at 6am" do
-        before do
-          travel_to Time.utc(2016, 11, 21, 14) do
-            described_class.run
-          end
-        end
-
-        it "does not send daily prompts" do
-          expect(enqueued_jobs.count).to eq(0)
-        end
-      end
-
-      context "at 7am" do
-        before do
-          travel_to Time.utc(2016, 11, 21, 15) do
-            described_class.run
-          end
-        end
-
-        it "sends a daily prompt to each team with unhandled responses" do
-          expect(unhandled_teams_count).to be > 0
-          expect(enqueued_jobs.count).to eq(unhandled_teams_count)
-          expect(enqueued_jobs.last[:args].first).to eq("SummaryMailer")
-        end
-      end
-
-      context "at 8am" do
-        before do
-          travel_to Time.utc(2016, 11, 21, 16) do
             described_class.run
           end
         end
