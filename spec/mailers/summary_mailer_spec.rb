3fef8dc4cd4a8dae3c6c13e2ac28117ce488ba1e
Andrew <andrew@huth.me>
2016-11-19 20:45:45 -0800

    Flatten summary mailer tests

--- spec/mailers/summary_mailer_spec.rb
+++ spec/mailers/summary_mailer_spec.rb
@@ -6,35 +6,35 @@ describe SummaryMailer, type: :mailer do
     let(:mail) { described_class.daily_summary_email(team) }
     let(:body) { mail.text_part.body.encoded }
 
-    context "when there are unhandled responses" do
+    it "sends to everyone on the team" do
+      expect(team.seats.count).to be > 0
+      expect(mail.to).to match_array(team.seats.pluck(:email))
+    end
+
+    context "for unhandled responses" do
       let(:unhandled_responses) { team.responses.unhandled }
 
       before do
         expect(unhandled_responses.count).to be > 0
       end
 
-      context "when there are handled responses" do
-        let(:handled_responses) { team.responses.where(handled: true) }
-
-        before do
-          expect(handled_responses.count).to be > 0
+      it "includes them" do
+        unhandled_responses.each do |response|
+          expect(body).to match(response.seat.name)
         end
+      end
+    end
 
-        it "sends to everyone on the team" do
-          expect(team.seats.count).to be > 0
-          expect(mail.to).to match_array(team.seats.pluck(:email))
-        end
+    context "for handled responses" do
+      let(:handled_responses) { team.responses.where(handled: true) }
 
-        it "includes unhandled responses" do
-          unhandled_responses.each do |response|
-            expect(body).to match(response.seat.name)
-          end
-        end
+      before do
+        expect(handled_responses.count).to be > 0
+      end
 
-        it "does not include handled responses" do
-          handled_responses.each do |response|
-            expect(body).to_not match(response.seat.name)
-          end
+      it "does not include them" do
+        handled_responses.each do |response|
+          expect(body).to_not match(response.seat.name)
         end
       end
     end
