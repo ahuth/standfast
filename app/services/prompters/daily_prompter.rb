708375934854dc599dc6c6b8496a5fe81287b47f
Andrew <andrew@huth.me>
2016-11-20 08:20:59 -0800

    Move all day/time logic out of the daily prompter and into the prompt scheduler
    Note that when checking the “weekend” we check to see if it’s sunday or
    monday, and not saturday or sunday. This is because we’re still
    hardcoding pacific time.

--- app/services/prompters/daily_prompter.rb
+++ app/services/prompters/daily_prompter.rb
@@ -1,21 +1,9 @@
 module Prompters
   class DailyPrompter
     def self.run
-      if !pacific_weekend_in_utc?
-        Team.find_each do |team|
-          PromptMailer.daily_status_email(team).deliver_later
-        end
+      Team.find_each do |team|
+        PromptMailer.daily_status_email(team).deliver_later
       end
     end
-
-    # This object will be ran everyday at 5:00 pm pacific time. However, we don't
-    # want to send it on weekends. The scheduler runs in UTC, and we need to take
-    # that into consideration. That time is currently the _next_ day in UTC.
-    def self.pacific_weekend_in_utc?
-      today = Date.today
-      today.sunday? || today.monday?
-    end
-
-    private_class_method :pacific_weekend_in_utc?
   end
 end
--- app/services/schedulers/prompt.rb
+++ app/services/schedulers/prompt.rb
@@ -1,13 +1,18 @@
 module Schedulers
   class Prompt
     def self.run
-      Prompters::DailyPrompter.run if its_time?
+      Prompters::DailyPrompter.run if !weekend? && its_time?
     end
 
     def self.its_time?
       Time.zone.now.hour == 1
     end
 
+    def self.weekend?
+      Time.zone.today.sunday? || Time.zone.today.monday?
+    end
+
     private_class_method :its_time?
+    private_class_method :weekend?
   end
 end
