2a477b302daca0bf3fb2cfa4642865825be6ba1a
Andrew <andrew@huth.me>
2016-11-22 20:36:18 -0800

    Use shoulda matchers in all of the model tests

--- spec/models/seat_spec.rb
+++ spec/models/seat_spec.rb
@@ -1,93 +1,11 @@
 require 'rails_helper'
 
 describe Seat, type: :model do
-  describe "validations" do
-    let(:seat) { seats(:bob_black_team_bill_seat) }
-
-    before do
-      expect(seat).to be_valid
-    end
-
-    context "without a name" do
-      before do
-        seat.name = nil
-      end
-
-      it "is invalid" do
-        expect(seat).to_not be_valid
-        expect(seat.errors.full_messages).to eq(["Name can't be blank"])
-      end
-    end
-
-    context "with a long name" do
-      before do
-        seat.name = "a" * 256
-      end
-
-      it "is invalid" do
-        expect(seat).to_not be_valid
-        expect(seat.errors.full_messages).to eq(["Name is too long (maximum is 255 characters)"])
-      end
-    end
-
-    context "without an email" do
-      before do
-        seat.email = nil
-      end
-
-      it "is invalid" do
-        expect(seat).to_not be_valid
-        expect(seat.errors.full_messages).to eq(["Email can't be blank"])
-      end
-    end
-
-    context "with a long email" do
-      before do
-        seat.email = "a" * 256
-      end
-
-      it "is invalid" do
-        expect(seat).to_not be_valid
-        expect(seat.errors.full_messages).to eq(["Email is too long (maximum is 255 characters)"])
-      end
-    end
-
-    context "without a team" do
-      before do
-        seat.team = nil
-      end
-
-      it "is invalid" do
-        expect(seat).to_not be_valid
-        expect(seat.errors.full_messages).to eq(["Team must exist"])
-      end
-    end
-
-    context "with the email of another seat from the same team" do
-      let(:other_seat) { seats(:bob_black_team_ted_seat) }
-
-      before do
-        expect(seat.team_id).to eq(other_seat.team_id)
-        seat.email = other_seat.email
-      end
-
-      it "is invalid" do
-        expect(seat).to_not be_valid
-        expect(seat.errors.full_messages).to eq(["Email has already been taken"])
-      end
-    end
-
-    context "with the email of another seat from a different team" do
-      let(:other_seat) { seats(:bob_pink_team_jill_seat) }
-
-      before do
-        expect(seat.team_id).to_not eq(other_seat.team_id)
-        seat.email = other_seat.email
-      end
-
-      it "is still valid" do
-        expect(seat).to be_valid
-      end
-    end
-  end
+  it { should belong_to(:team) }
+  it { should have_many(:responses).dependent(:destroy) }
+  it { should validate_presence_of(:name) }
+  it { should validate_length_of(:name).is_at_most(255) }
+  it { should validate_presence_of(:email) }
+  it { should validate_length_of(:email).is_at_most(255) }
+  it { should validate_uniqueness_of(:email).scoped_to(:team_id) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,14 @@
 require 'rails_helper'
 
 describe User, type: :model do
+  it { should belong_to(:account) }
+  it { should validate_presence_of(:email) }
+
   describe "validations" do
     let(:user) { users(:jane) }
 
     before do
       expect(user).to be_valid
-    end
-
-    context "without an email" do
-      before do
-        user.email = nil
-      end
-
-      it "is invalid" do
-        expect(user).to_not be_valid
-        expect(user.errors.full_messages).to eq(["Email can't be blank"])
-      end
     end
 
     context "with an email that already exists" do
