b3f6b0c5064a41cfa1766aa958d98628721bd5d0
Andrew <andrew@huth.me>
2016-11-22 20:35:59 -0800

    Configure shoulda for rspec and model tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,3 +61,11 @@ RSpec.configure do |config|
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
 end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :active_record
+    with.library :active_model
+  end
+end
