54da2192f4b5445a6c6ee4872620da2ef7838eb6
Andrew <andrew@huth.me>
2016-11-26 06:50:42 -0800

    Add index to the time_zone column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161124223341) do
+ActiveRecord::Schema.define(version: 20161126023042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20161124223341) do
     t.string   "time_zone",              null: false
     t.index ["account_id", "name"], name: "index_teams_on_account_id_and_name", unique: true, using: :btree
     t.index ["account_id"], name: "index_teams_on_account_id", using: :btree
+    t.index ["time_zone"], name: "index_teams_on_time_zone", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
