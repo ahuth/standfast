38d6c2e9462d4564dbbb8d8b57ee28564c6e3755
Andrew <andrew@huth.me>
2016-11-30 21:45:23 -0800

    Use ES6 features

--- app/assets/javascripts/active.js
+++ app/assets/javascripts/active.js
@@ -1,12 +1,12 @@
 // Find every element with a `data-active` attribute. If the attribute is equal
 // to the current path of the URL, set an `active` class on it.
 document.addEventListener("turbolinks:load", function (event) {
-  var elements = document.querySelectorAll("[data-active]");
-  var path = window.location.pathname;
+  let elements = document.querySelectorAll("[data-active]");
+  let path = window.location.pathname;
   elements.forEach(function (element) {
-    var paths = element.dataset.active.split(",");
-    var patterns = paths.map(function (path) { return path + ".*"; });
-    var pattern = new RegExp(patterns.join("|"));
+    let paths = element.dataset.active.split(",");
+    let patterns = paths.map(function (path) { return path + ".*"; });
+    let pattern = new RegExp(patterns.join("|"));
     if (pattern.test(path)) {
       element.classList.add("active");
     }
--- app/assets/javascripts/confirm.js
+++ app/assets/javascripts/confirm.js
@@ -2,7 +2,7 @@
 // after clicking. The value of the `data-confirm` attribute will be shown in
 // the dialog, and canceling should prevent the action from taking place.
 document.addEventListener("turbolinks:load", function (event) {
-  var elements = document.querySelectorAll("[data-confirm]");
+  let elements = document.querySelectorAll("[data-confirm]");
   elements.forEach(function (element) {
     element.onclick = function (event) {
       return window.confirm(event.target.dataset.confirm);
