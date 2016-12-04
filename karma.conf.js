a6c5f3acd0d01b587dfa6fd02e2398f4210dfddb
Andrew <andrew@huth.me>
2016-12-03 16:55:30 -0800

    Report lint failures in the test results

--- karma.conf.js
+++ karma.conf.js
@@ -22,7 +22,7 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'spec/javascripts/**/*-spec.js': ['browserify']
+      'spec/javascripts/**/*-spec.js': ['eslint', 'browserify']
     },
 
     // test results reporter to use
