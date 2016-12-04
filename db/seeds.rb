f23b3c5a17efa46eda5f5a7be8b8e5285d826847
Andrew <andrew@huth.me>
2016-12-03 18:48:05 -0800

    Use double quotes in the db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,25 +5,25 @@ if Rails.env.development?
   # User
   jane = User.find_or_initialize_by({
     account_id: account.id,
-    email: 'jane@example.com'
+    email: "jane@example.com"
   })
-  jane.password = 'password'
-  jane.encrypted_password = User.new.send(:password_digest, 'password')
+  jane.password = "password"
+  jane.encrypted_password = User.new.send(:password_digest, "password")
   jane.save!
 
   # Teams
-  blue_team = Team.find_or_create_by!(name: 'Blue', account_id: account.id, time_zone: "Pacific Time (US & Canada)")
-  red_team = Team.find_or_create_by!(name: 'Red', account_id: account.id, time_zone: "Pacific Time (US & Canada)")
+  blue_team = Team.find_or_create_by!(name: "Blue", account_id: account.id, time_zone: "Pacific Time (US & Canada)")
+  red_team = Team.find_or_create_by!(name: "Red", account_id: account.id, time_zone: "Pacific Time (US & Canada)")
 
   # Seats
-  Seat.find_or_create_by!(name: 'Adam', email: 'adam@example.com', team_id: blue_team.id)
-  Seat.find_or_create_by!(name: 'Eric', email: 'eric@example.com', team_id: blue_team.id)
-  ryan_seat = Seat.find_or_create_by!(name: 'Ryan', email: 'ryan@example.com', team_id: blue_team.id)
-  shirish_seat = Seat.find_or_create_by!(name: 'Shirish', email: 'shirish@example.com', team_id: blue_team.id)
-  anna_seat = Seat.find_or_create_by!(name: 'Anna', email: 'anna@example.com', team_id: blue_team.id)
-  Seat.find_or_create_by!(name: 'Goose', email: 'goose@example.com', team_id: red_team.id)
-  Seat.find_or_create_by!(name: 'Iceman', email: 'iceman@example.com', team_id: red_team.id)
-  Seat.find_or_create_by!(name: 'Maverick', email: 'maverick@example.com', team_id: red_team.id)
+  Seat.find_or_create_by!(name: "Adam", email: "adam@example.com", team_id: blue_team.id)
+  Seat.find_or_create_by!(name: "Eric", email: "eric@example.com", team_id: blue_team.id)
+  ryan_seat = Seat.find_or_create_by!(name: "Ryan", email: "ryan@example.com", team_id: blue_team.id)
+  shirish_seat = Seat.find_or_create_by!(name: "Shirish", email: "shirish@example.com", team_id: blue_team.id)
+  anna_seat = Seat.find_or_create_by!(name: "Anna", email: "anna@example.com", team_id: blue_team.id)
+  Seat.find_or_create_by!(name: "Goose", email: "goose@example.com", team_id: red_team.id)
+  Seat.find_or_create_by!(name: "Iceman", email: "iceman@example.com", team_id: red_team.id)
+  Seat.find_or_create_by!(name: "Maverick", email: "maverick@example.com", team_id: red_team.id)
 
   # Responses
   Response.find_or_create_by!(handled: false, seat_id: shirish_seat.id, body: <<~EOS)
